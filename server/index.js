e0c9db767de9ac59e6e8350727faa146eb1c20d6
Dawn <dawnsaq@gmail.com>
2019-03-13 10:51:46 -0700

    updated parser var

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3002;
 
 app.use(bodyParser.json());
 
-app.use(parser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
